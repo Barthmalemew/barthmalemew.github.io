03fde413abbdcacb9ad579104667c40b2777ea7f
barthmalemew <kevinrouse105@gmail.com>
2024-05-16 00:26:18 -0400

    The Big Update

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -185,6 +185,13 @@ declare module 'astro:content' {
 
 	type ContentEntryMap = {
 		"blog": {
+"post1.md": {
+	id: "post1.md";
+  slug: "post1";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
 "post2.md": {
 	id: "post2.md";
   slug: "post2";
